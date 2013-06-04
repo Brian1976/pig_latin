6059994eb048a97aba4a28b0813b29b0ea3d2df1
noam <clusterfoo@server.fake>
2013-06-04 14:56:59 -0400

    All Done. Fixed bug about return value in each statement.

--- pig_latin/converter.rb
+++ pig_latin/converter.rb
@@ -1,18 +1,24 @@
 module PigLatin
+  
+  # This class is in charge of converting an input word into pig latin
   class Converter
     VOWELS = ["a", "e", "i", "o", "u"]
 
     def self.convert(input_word)
-      word = input_word.downcase
-      word_length = word.length
+      word           = input_word.downcase
+      word_length    = word.length
       vowel_position = find_vowel(word)
 
-      puts (word[vowel_position...word_length] + word[0...vowel_position] + "ay")
+      output         = word[vowel_position...word_length] + \
+                       word[0...vowel_position] + "ay"
+      output
     end
+
+    private
 
     def self.find_vowel(word)
       i = 0
-      until word.include? word[i]
+      until VOWELS.include? word[i]
         i += 1
       end
 
--- pig_latin/user_interface.rb
+++ pig_latin/user_interface.rb
@@ -1,3 +1,33 @@
 module PigLatin
+
+  # This class is responsible for handling user input.
   class UI
+    # Runs pig_latin.rb as a command line utility that takes user arguments
+    # as its input
+    def self.run_as_command_line_utility(words)
+      if ARGV.empty?
+        puts "! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !"
+        puts "!                                                 !"
+        puts "! correct use: pig_latin.rb word_1 word_2 word_3  !"
+        puts "!                                                 !"
+        puts "! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !"
+      else
+        puts convert_words(words)
+      end
+    end
+
+    def self.convert_words(words)
+      converted_words = []
+      words.each do |word|
+        converted_words << convert(word)
+      end
+      converted_words
+    end
+
+  private
+
+    def self.convert(word)
+      Converter.convert(word)
+    end
+  end
 end
